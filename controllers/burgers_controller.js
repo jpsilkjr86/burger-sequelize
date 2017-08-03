ffa34834d84989c77accc1a6125dcfd3c75b5611
John Silkey <jpsilkjr86@gmail.com>
2017-08-03 16:22:14 -0700

    finished refactoring burgers_controllers.js to use sequelize for all CRUD instead of old orm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -52,65 +52,30 @@ module.exports = (app) => {
 			
 		});
 	});
+	// route for eating
+	app.post('/burgers/eat', (req, res) => {
+		db.Burger.update({devoured: true}, {
+			where: {
+				burger_name: req.body.burger_name
+			}
+		}).then(result => {
+			res.send(result);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
+	// route for ordering more
+	app.post('/burgers/ordermore', (req, res) => {
+		db.Burger.update({devoured: false}, {
+			where: {
+				burger_name: req.body.burger_name
+			}
+		}).then(result => {
+			res.send(result);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
 };
-
-// ==========   OLD   ==========
-
-// // route for rendering html to index via handlebars engine
-// 	app.get('/', (req, res) => {
-// 		burger.all().then(results => {
-// 			// get ready burgers and eaten burgers array by sifting through data
-// 			let eatenBurgers = [];
-// 			let readyBurgers = [];
-// 			// loop through results and sort burgers according to devoured value
-// 			for (let i = 0; i < results.length; i++) {
-// 				if (results[i].devoured) {
-// 					eatenBurgers.push(results[i]);
-// 				}
-// 				if (!results[i].devoured) {
-// 					readyBurgers.push(results[i]);
-// 				}
-// 			}
-// 			// render according to arrays
-// 			res.render('index', {
-// 				eatenBurgers: eatenBurgers,
-// 				readyBurgers: readyBurgers
-// 			});
-// 		});		
-// 	});
-// 	// route for grabbing all burger data
-// 	app.get('/burgers/all', (req, res) => {
-// 		burger.all().then(results => {
-// 			res.json(results);
-// 		}).catch(err => {
-// 			res.send(err);
-// 			console.log(err);
-// 		});
-// 	});
-// 	// route for posting new burger data
-// 	app.post('/burgers/new', (req, res) => {
-// 		burger.addNew(req.body.burger_name).then(result => {
-// 			res.send(result);
-// 		}).catch(err => {
-// 			res.send(err);
-// 			console.log(err);
-// 		});
-// 	});
-// 	// route for eating
-// 	app.post('/burgers/eat', (req, res) => {
-// 		burger.eat(req.body.burger_name).then(result => {
-// 			res.send(result);
-// 		}).catch(err => {
-// 			res.send(err);
-// 			console.log(err);
-// 		});
-// 	});
-// 	// route for ordering more
-// 	app.post('/burgers/ordermore', (req, res) => {
-// 		burger.orderMore(req.body.burger_name).then(result => {
-// 			res.send(result);
-// 		}).catch(err => {
-// 			res.send(err);
-// 			console.log(err);
-// 		});
-// 	});
