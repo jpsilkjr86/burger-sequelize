7adaf1e6c434d39c72dc8417099c8c1cac2f3cd5
John Silkey <jpsilkjr86@gmail.com>
2017-08-03 16:17:23 -0700

    changed burgerControllers to use sequelize instead of old orm for rending the main page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,38 @@
 const db = require('../models');
 // exports as a function which takes in express app paramters
 module.exports = (app) => {
+	// route for rendering html to index via handlebars engine
+	app.get('/', (req, res) => {
+		// sequelize method for returning all rows
+		db.Burger.findAll({}).then(results => {
+			// get ready burgers and eaten burgers array by sifting through data
+			let eatenBurgers = [];
+			let readyBurgers = [];
+			// loop through results and sort burgers according to devoured value
+			for (let i = 0; i < results.length; i++) {
+				if (results[i].devoured) {
+					eatenBurgers.push(results[i]);
+				}
+				if (!results[i].devoured) {
+					readyBurgers.push(results[i]);
+				}
+			}
+			// render according to arrays
+			res.render('index', {
+				eatenBurgers: eatenBurgers,
+				readyBurgers: readyBurgers
+			});
+		});		
+	});
+	// route for grabbing all burger data and sending is as json
+	app.get('/burgers/all', (req, res) => {
+		db.Burger.all().then(results => {
+			res.json(results);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
 	// route for posting new burger data
 	app.post('/burgers/new', (req, res) => {
 		console.log(req.body);
